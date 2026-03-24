3f3c64adfff13a5bd5b1633575cd1a65bcb6b9e3
“SushantKumar29” <sushantkmr29@gmail.com>
2026-03-24 08:09:54 +0530

    fix: Update husky and commit lint to add check before commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+export default {
   '*.{ts,js}': ['eslint --fix', 'prettier --write'],
   '*.{json,md}': ['prettier --write'],
+  '*/src/**/*.{ts,js}': ['eslint --fix', 'prettier --write'], // This line will format TypeScript files in all workspace directories
 };
